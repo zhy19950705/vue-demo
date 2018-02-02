e3694a165bb1e70b1d905475d8ed5912ea963ab2
zhy <1144567324@qq.com>
2018-02-02 16:59:51 +0800

    难受难受

---
+++ src/views/dashboard/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="dashboard-container">
+      <component v-bind:is="currentRole"></component>
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: "index"
+    }
+</script>
+
+<style scoped>
+
+</style>
